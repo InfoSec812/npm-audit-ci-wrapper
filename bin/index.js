2ef5acb4097ff83b346ef83117494fb84347892a
Deven Phillips <deven.phillips@redhat.com>
2019-02-15 12:44:00 -0500

    Refactored for testing and coverage

---
+++ bin/index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+/**
+ * Copyright [2018] [Joseph B. Phillips]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
+const { exec } = require('child_process');
+const { parse_audit_results } = require('../lib/parser');
+const { parse_args } = require('../lib/parse_args');
+
+const { threshold, ignoreDev, json_output } = parse_args();
+
+if ( args.options.hasOwnProperty('registry') ) {
+  registry = args.options.registry; // Set the registry
+}
+
+// Build the npm audit command
+command = 'npm audit --json'
+if( typeof registry !== 'undefined' ) {
+  command += ' --registry=' + registry
+}
+
+//
+// Execute and capture the output for processing
+exec(command, {maxBuffer: 500 * 1024}, (err, stdout, stderr) => {
+  process.exit(parse_audit_results(err, stdout, stderr, threshold, ignoreDev, json_output));
+});
